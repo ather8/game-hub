1e1f597921ca013841ce20df00a0436f1cdf9e2f
Ather Sayed <athersayed8@gmail.com>
2023-10-30 16:40:23 +0200

    Sort the games

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -16,7 +16,7 @@ export interface Game {
     metacritic: number;
 }
 
-const useGames = (gameQuery: GameQuery) => useData<Game>('/games',{params: {genres:gameQuery.genre?.id, platforms: gameQuery.platform?.id}},
+const useGames = (gameQuery: GameQuery) => useData<Game>('/games',{params: {genres:gameQuery.genre?.id, platforms: gameQuery.platform?.id,ordering: gameQuery.sortOrder}},
 [gameQuery]);
 
 export default useGames;
